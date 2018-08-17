f4f8ba88ffbd6011386db1ce9ece12cab1bddfa3
Christian Alejandro Herrejon Villa <christianahvilla@gmail.com>
2018-08-17 15:32:01 -0500

    Initial commit

---
+++ scripts/app.js
@@ -0,0 +1,74 @@
+(function() {
+    'use strict';
+    var app = {
+        users: [],
+        names: ['misael93','JoseATP', 'baltaITIC','adjaf','lgruelas','daniboygs','christianahvilla','Diego2218'],
+        container: document.querySelector('.cards'),
+        articleTemplate: document.querySelector('.friend'),
+    };
+
+
+    app.displayFriens = function() {
+        // remove current news
+        app.container.innerText = "";
+        app.users.forEach(function(item) {
+            var article = app.articleTemplate.cloneNode(true);
+            console.log(item.name);
+            article.querySelector('.avatar').src = item.avatar_url || '';
+            article.querySelector('.name').textContent = item.name;
+            article.querySelector('.bio').textContent = item.bio;
+            article.removeAttribute('hidden');
+            app.container.appendChild(article);
+        });
+
+        document.body.classList.remove("loading");
+    }
+
+    app.loadFriends = function() {
+        document.body.classList.add("loading");
+        app.news = [];
+
+        for(var j=0; j< app.names.length; j++){
+          var url = 'https://api.github.com/users/'+app.names[j];
+
+          if ('caches' in window) {
+              caches.match(url).then(function(response) {
+                  if (response) {
+                      response.json().then(function updateFromCache(json) {
+                          app.users.push(json);
+                      });
+                  }
+              });
+          }
+          var xhttp = new XMLHttpRequest();
+          xhttp.onreadystatechange = function() {
+              if (this.readyState == 4 && this.status == 200) {
+                  app.users.push(JSON.parse(this.response));
+              }
+          };
+          xhttp.open("GET", url, true);
+          xhttp.send();
+        };
+        app.displayFriens();
+    }
+
+    app.init = function() {
+      console.log('AQUI ENTRA');
+        app.loadFriends();
+    }
+
+    document.getElementById('reload-git-btn').addEventListener('click', function() {
+        app.loadFriends();
+    })
+    // start app
+    app.init();
+
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker
+            .register('./service-worker.js')
+            .then(function(registration) {
+                console.log('Service Worker Registered', registration.scope);
+            });
+    }
+
+})();
---
+++ service-worker.js
@@ -0,0 +1,59 @@
+var cacheName = 'git-1.0.0';
+var dataCacheName = 'git-data-v1';
+
+var urlsToCache = [
+  './',
+  './index.html',
+  './scripts/app.js',
+  './styles/style.css',
+  './imgs/logo.png',
+  './imgs/loader.svg',
+  './imgs/reload.svg'
+];
+
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('activate', function(e) {
+  console.log('[ServiceWorker] Activate');
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (key !== cacheName && key !== dataCacheName) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+  console.log('[Service Worker] Fetch', e.request.url);
+  var dataUrl = 'https://api.github.com/users/';
+  if (e.request.url.indexOf(dataUrl) > -1) {
+     console.log('error');
+    e.respondWith(
+      caches.open(dataCacheName).then(function(cache) {
+        return fetch(e.request).then(function(response){
+          cache.put(e.request.url, response.clone());
+          return response;
+        });
+      })
+    );
+  } else {
+    e.respondWith(
+      caches.match(e.request).then(function(response) {
+        return response || fetch(e.request);
+      })
+    );
+  }
+});
